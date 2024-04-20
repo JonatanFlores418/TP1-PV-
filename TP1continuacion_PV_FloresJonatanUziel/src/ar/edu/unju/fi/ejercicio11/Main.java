e29db9b7b6f84efe97e8aea298add5e5257a0ff7
bangho <bangho@DESKTOP-7O821GF>
2024-04-20 20:08:21 -0300

    punto 11

--- TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio11/Main.java
+++ TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -3,7 +3,7 @@ package ar.edu.unju.fi.ejercicio11;
 public class Main {
     public static void main(String[] args) {
     	int j = 40;
-        
+          
     	for (int i = 2; j >= 0; i += 4) {
             j -= 3;
             int value = 40 + 37 * i - 3 * j * (i - 2);
