dc583f736f581ee1dfd6e7dbe3a273aee6994ed3
bangho <bangho@DESKTOP-7O821GF>
2024-04-20 20:27:05 -0300

    punto 15

--- TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio15/Main.java
+++ TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio15/Main.java
@@ -1,10 +1,34 @@
 package ar.edu.unju.fi.ejercicio15;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int size;
+      do {
+            System.out.print("Ingrese un número entero en el rango [5, 10]: ");
+            size = scanner.nextInt();
+        } while (size < 5 || size > 10);
 
-	}
+        String[] nombres = new String[size];
 
+        	for (int i = 0; i < size; i++) {
+        		System.out.print("Ingrese un nombre para la posición " + i + ": ");
+        		nombres[i] = scanner.next();
+        }
+
+        System.out.println("\nContenido del array (comenzando desde el primer índice):");
+        	for (int i = 0; i < size; i++) {
+        		System.out.println("Posición " + i + ": " + nombres[i]);
+        }
+
+        System.out.println("\nContenido del array (comenzando desde el último índice):");
+        	for (int i = size - 1; i >= 0; i--) {
+        		System.out.println("Posición " + i + ": " + nombres[i]);
+        }
+
+  scanner.close();
+    }
 }
