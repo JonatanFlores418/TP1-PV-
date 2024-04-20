93c9bfd320d42e83fa7cc7695182ddd22642379a
bangho <bangho@DESKTOP-7O821GF>
2024-04-20 20:20:14 -0300

    punto 13

--- TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio13/Main.java
+++ TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -1,10 +1,23 @@
 package ar.edu.unju.fi.ejercicio13;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int[] numeros = new int[8];
 
-	}
+     for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingrese el número para la posición " + i + ": ");
+            numeros[i] = scanner.nextInt();
+        }
 
+     System.out.println("\nElementos del array:");
+     for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Índice " + i + ": " + numeros[i]);
+        }
+
+  scanner.close(); 
+    }
 }
