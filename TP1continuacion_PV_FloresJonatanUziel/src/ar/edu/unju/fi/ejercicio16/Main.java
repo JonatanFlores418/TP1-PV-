361fdf2401a35e6d87c9e9218f85f434f86d1ecf
bangho <bangho@DESKTOP-7O821GF>
2024-04-20 20:32:41 -0300

    punto 16

--- TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio16/Main.java
+++ TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio16/Main.java
@@ -1,10 +1,44 @@
 package ar.edu.unju.fi.ejercicio16;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String[] nombres = new String[5];
 
-	}
+        System.out.println("Ingrese nombres de personas:");
+        	for (int i = 0; i < nombres.length; i++) {
+        		System.out.print("Nombre " + (i + 1) + ": ");
+        		nombres[i] = scanner.nextLine();
+        }
 
+        System.out.println("\nValores guardados en el array:");
+        int index = 0;
+        	while (index < nombres.length) {
+        		System.out.println("Índice " + index + ": " + nombres[index]);
+        		index++;
+        }
+
+        System.out.println("\nTamaño del array: " + nombres.length);
+
+        byte indiceAEliminar;
+        	do {
+        		System.out.print("Ingrese el índice del elemento a eliminar (0-4): ");
+        		indiceAEliminar = scanner.nextByte();
+        } while (indiceAEliminar < 0 || indiceAEliminar >= nombres.length);
+
+        	for (int i = (int)indiceAEliminar; i < nombres.length - 1; i++) {
+        		nombres[i] = nombres[i + 1];
+        }
+        nombres[nombres.length - 1] = "";
+
+        System.out.println("\nArreglo después de eliminar el elemento:");
+        	for (int i = 0; i < nombres.length; i++) {
+        		System.out.println("Índice " + i + ": " + nombres[i]);
+        }
+
+  scanner.close();
+    }
 }
