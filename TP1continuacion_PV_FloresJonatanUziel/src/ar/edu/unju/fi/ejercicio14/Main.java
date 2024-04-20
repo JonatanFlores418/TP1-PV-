7150dcb3b36ffe5a806a3bf9f24bce82e4b754e8
bangho <bangho@DESKTOP-7O821GF>
2024-04-20 20:21:57 -0300

    punto 14

--- TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio14/Main.java
+++ TP1continuacion_PV_FloresJonatanUziel/src/ar/edu/unju/fi/ejercicio14/Main.java
@@ -1,10 +1,35 @@
 package ar.edu.unju.fi.ejercicio14;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int size;
+        do {
+            System.out.print("Ingrese un número entero en el rango [3, 10]: ");
+            size = scanner.nextInt();
+        } while (size < 3 || size > 10);
 
-	}
+        int[] array = new int[size];
 
+        for (int i = 0; i < size; i++) {
+            System.out.print("Ingrese un número entero para la posición " + i + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("\nValores del array:");
+        for (int i = 0; i < size; i++) {
+            System.out.println("Posición " + i + ": " + array[i]);
+        }
+
+        int suma = 0;
+        for (int i = 0; i < size; i++) {
+            suma += array[i];
+        }
+
+        System.out.println("\nSuma de todos los valores: " + suma);
+
+        scanner.close();
+    }
 }
